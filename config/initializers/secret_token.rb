12c62f292e1185cd415a9fc765893368296bb6be
Sean Darcy <darcys22@gmail.com>
2014-02-16 09:14:57 -0800

    original rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mav1::Application.config.secret_key_base = '582f3480c70b25a7450bc4b23c2aca87b448686be14954dc5cfcc2e2770d54a54f04d9a7322f8a22903147868045e248659085d4648d3c0fa5d9fd059b9cf9f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mav1::Application.config.session_store :cookie_store, key: '_mav1_session'
