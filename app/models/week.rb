770aa8ea3f28832d8ea83299ce84f938ea9d929e
Sean Darcy <darcys22@gmail.com>
2014-03-07 12:21:29 -0800

    remove debugging

--- app/models/week.rb
+++ app/models/week.rb
@@ -17,7 +17,6 @@ class Week < ActiveRecord::Base
     end
 
     def self.render
-      byebug
       availability = ::MotionlessAgitator::EmployeeAvailability.new
       required_hours = ::MotionlessAgitator::WeeklyDemand.new
       ::MotionlessAgitator::Renderer.new(availability, required_hours).render!
