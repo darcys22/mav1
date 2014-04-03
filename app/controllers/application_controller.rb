60bfd7865e492bb10359ddb5c98a47f6cfa967c3
Sean Darcy <darcys22@gmail.com>
2014-04-03 16:22:30 -0700

    timezone fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
 
   
-  #before_filter :set_time_zone
+  around_filter :set_time_zone, :if => :current_user
 
-  #def set_time_zone
-    #Time.zone = current_user.time_zone if current_user
-  #end
+  def set_time_zone(&block)
+    Time.use_zone(current_user.timezone, &block)
+  end
 
   @overcapacity = false
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
   end
+
+  def edit
+    respond_to do |format|
+      if current_user.update(user_params)
+        format.html { redirect_to root_url, notice: 'Employee was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'show' }
+      end
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:timezone)
+  end
+
 end
