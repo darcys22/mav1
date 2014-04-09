749c63216f3b460034fbd38f6bed666459b1f168
Sean Darcy <darcys22@gmail.com>
2014-04-08 19:05:38 -0700

    Time.find_zone.. wish i knew that ages ago

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+  before_filter :set_overcapacity
 
-  
   around_filter :set_time_zone, :if => :current_user
 
+
   def set_time_zone(&block)
-    Chronic.time_class = current_user.timezone
-    Time.use_zone(current_user.timezone, &block)
+    Chronic.time_class = Time.find_zone(current_user.timezone)
+    Time.use_zone(current_user.timezone, &block) 
   end
 
-  @overcapacity = false
+
+  def set_overcapacity
+    @overcapacity = false
+  end
+
 end
