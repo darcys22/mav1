4d826e5611bc5969dc248af15f1d00233c28477b
Sean Darcy <darcys22@gmail.com>
2014-05-15 15:22:41 -0700

    current user in model, Week and Schedule

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,21 @@ class ApplicationController < ActionController::Base
   before_filter :set_overcapacity
 
   around_filter :set_time_zone, :if => :current_user
+  around_filter :set_current_user
 
 
   def set_time_zone(&block)
     Chronic.time_class = Time.find_zone(current_user.timezone)
     Time.use_zone(current_user.timezone, &block) 
+  end
+
+  def set_current_user
+    Schedule.current_user = current_user
+    Week.current_user = current_user
+    yield
+  ensure
+    Schedule.current_user = nil
+    Week.current_user = nil
   end
 
 
