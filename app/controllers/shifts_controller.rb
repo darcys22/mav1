d5526584672a40b1680144126ea3fb931dff077a
Sean Darcy <darcys22@gmail.com>
2014-08-09 05:31:25 -0700

    half arsed attempt at making the shift update form

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -13,9 +13,13 @@ class ShiftsController < ApplicationController
   end
 
   def show
+    @shift = current_user.shifts.find(params[:shift_id])
+    @emp = @shift.shiftable_type == "Employee" ? @shift.shiftable_id : nil
   end
 
   def update
+    @shift = current_user.shifts.find(params[:shift_id])
+    @shift.update_attributes(params[:shift])
   end
 
   def destroy
