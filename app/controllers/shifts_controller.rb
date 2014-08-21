b0de2bba16481fbf0681fca5f6d8a6d1a7797403
Sean Darcy <darcys22@gmail.com>
2014-08-21 04:19:23 -0700

    so nils linking is shit

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,7 +1,7 @@
 class ShiftsController < ApplicationController
   def update
-    @shift = current_user.shifts.find(params[:shift_id])
-    @emp = @shift.shiftable_type == "Employee" ? @shift.shiftable_id : nil
-    #@shift.update_attributes(params[:shift])
+    #@shift = current_user.shifts.find(params[:shift_id])
+    #@emp = @shift.shiftable_type == "Employee" ? @shift.shiftable_id : nil
+    ##@shift.update_attributes(params[:shift])
   end
 end
